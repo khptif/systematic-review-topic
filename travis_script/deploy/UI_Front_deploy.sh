6405baf38f1c2b79c866bd464d3321b9fa5cb39b
Fatih.Sarikaya <git@gitlab.unige.ch:Fatih.Sarikaya/semantique_2021.git>
2022-03-24 15:31:59 +0100

    travis_deploy

--- travis_script/deploy/UI_Front_deploy.sh
+++ travis_script/deploy/UI_Front_deploy.sh
@@ -14,7 +14,14 @@ command_rm="sudo docker rm -f khptif/UI-Front:latest"
 command_start="sudo docker container run --env-file=docker_environnment.env --name=ui_front -d -p 8000:8000 khptif/UI-Front:latest"
 
 adresse=${machine_front_user}@${machine_front_ip}
+id_key=~/.ssh/id_rsa_key
 
-echo | ssh -o StrictHostKeyChecking=no -i id_key $adresse $command_stop
-ssh -o StrictHostKeyChecking=no -i id_key $adresse $command_rm
-ssh -o StrictHostKeyChecking=no -i id_key $adresse $command_start
+echo ${UI_Front_key} > id_key
+chmod 600 id_key
+
+eval 'ssh-agent -s'
+ssh-add id_key
+
+ssh -o StrictHostKeyChecking=no -i $id_key $adresse $command_stop
+ssh -o StrictHostKeyChecking=no -i $id_key $adresse $command_rm
+ssh -o StrictHostKeyChecking=no -i $id_key $adresse $command_start
