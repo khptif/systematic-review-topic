155aa52a2a5e01bafaa0049e1ec14db198f4db32
Fatih.Sarikaya <git@gitlab.unige.ch:Fatih.Sarikaya/semantique_2021.git>
2022-05-25 18:18:53 +0200

    debug_deploy_beta

--- Install_Machine/Machine_Back/parametres_machine.sh
+++ Install_Machine/Machine_Back/parametres_machine.sh
@@ -5,6 +5,8 @@ export user_name=ubuntu
 export host_adresse=195.15.241.83
 export host_port=8000
 
+export host_domain=backend.systematic-review-topic-test.ch
+
 export is_database=False
 export is_backend=True
 export is_frontend=False
