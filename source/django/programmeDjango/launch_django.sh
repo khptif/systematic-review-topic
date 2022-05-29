fdcdf0144305a74f3551daa0e1e545313fd028ef
Fatih.Sarikaya <git@gitlab.unige.ch:Fatih.Sarikaya/semantique_2021.git>
2022-05-29 15:32:25 +0200

    change from decentralised to centralised

--- source/django/programmeDjango/launch_django.sh
+++ source/django/programmeDjango/launch_django.sh
@@ -38,6 +38,6 @@ python3 manage.py migrate
 #uwsgi --socket django.sock --module programmeDjango.wsgi --daemonize=./docker_volume/log.log
 
 #while true; do sleep 1000; done
-#python3 manage.py runserver 127.0.0.1:8000
+python3 manage.py runserver 127.0.0.1:8000
 
-cat settings_path
+
