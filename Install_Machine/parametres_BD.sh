35c9b2a6aed65bdbeb7183a6133ffdbbefb69c78
Fatih.Sarikaya <git@gitlab.unige.ch:Fatih.Sarikaya/semantique_2021.git>
2022-06-29 12:49:29 +0200

    debug

--- Install_Machine/parametres_BD.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-
-export db_name=test			# nom de la base de donnée
-export db_user_name=fatih		# nom de l'utilisateur de la base de donnée
-export password=1234			# mot de passe de la base de donnée
-
